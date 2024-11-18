6ac5e2c30269c5ed3263345b29ad312b7e1c098b
Emmanuel Girardin <emmanuel.girardin@outlook.com>
2024-11-18 22:18:58 +0100

    MOD: clean barycentric

--- rubikscross/mesh_tools/inverse_map_barycentric.py
+++ rubikscross/mesh_tools/inverse_map_barycentric.py
@@ -3,6 +3,7 @@ import math
 
 import numba
 
+
 @numba.njit()
 def vertex_index_buffer(h: int, w: int):
     N = (h - 1) * (w - 1) * 2
@@ -19,11 +20,13 @@ def vertex_index_buffer(h: int, w: int):
             n += 2
     return triangle_vib
 
-@numba.njit()
-def invert_map(xmap, ymap):
+
+@numba.jit(nopython=True)
+def invert_map(xmap, ymap, diagnostic=False):
     h, w = xmap.shape
-    xmap_inv = np.zeros_like(xmap, dtype=np.float32) - 1
-    ymap_inv = np.zeros_like(ymap, dtype=np.float32) - 1
+    xmap_inv = np.zeros_like(xmap) - 1
+    ymap_inv = np.zeros_like(ymap) - 1
+    TN = np.zeros(shape=(h, w), dtype=np.int32)
 
     triangle_vib = vertex_index_buffer(h, w)
 
@@ -53,48 +56,40 @@ def invert_map(xmap, ymap):
         j2 = k2 % w
 
         # find surrounding rectangle
-        xmin = min(min(x0, x1), x2)
-        ymin = min(min(y0, y1), y2)
-        xmax = max(max(x0, x1), x2)
-        ymax = max(max(y0, y1), y2)
+        xmin = int(math.floor(min(x0, x1, x2)))
+        ymin = int(math.floor(min(y0, y1, y2)))
+        xmax = int(math.ceil(max(x0, x1, x2)))
+        ymax = int(math.ceil(max(y0, y1, y2)))
 
-        xmin = max(int(math.floor(xmin)), 0)
-        ymin = max(int(math.floor(ymin)), 0)
-        xmax = min(int(math.ceil(xmax)), w - 1)
-        ymax = min(int(math.ceil(ymax)), h - 1)
-        # xmax += 1
-        # ymax += 1
+        xmin = min(max(0, xmin), w-1)
+        ymin = min(max(0, ymin), h-1)
+        xmax = min(max(0, xmax), w-1)
+        ymax = min(max(0, ymax), h-1)
+
+        if norm == 0:
+            xmap_inv[ymin:ymax, xmin:xmax] = j0
+            ymap_inv[ymin:ymax, xmin:xmax] = i0
+            TN[ymin:ymax, xmin:xmax] += 1
+            continue
+
         for px in range(xmin, xmax):
             pwx0 = y12 * (px - x2)
             pwx1 = -y02 * (px - x2)
             for py in range(ymin, ymax):
-                if norm == 0:
-                    xmap_inv[py, px] = j0
-                    ymap_inv[py, px] = i0
-                else:
-                    # compute non-normalized weights of barycentric coordinates
-                    w0 = (pwx0 + x21 * (py - y2))/norm
-                    w1 = (pwx1 + x02 * (py - y2))/norm
-                    w2 = 1 - w0 - w1
-                    #
-                    # w0 = 1
-                    # w1 = 0
-                    # w2 = 0
-                    # norm = 1
+                # compute non-normalized weights of barycentric coordinates
+                w0 = (pwx0 + x21 * (py - y2)) / norm
+                w1 = (pwx1 + x02 * (py - y2)) / norm
+                w2 = 1 - w0 - w1
 
-                    if w0 < 0 or w1 < 0 or w2 < 0:
-                        continue
+                # if w0 < 0 or w1 < 0 or w2 < 0:
+                #     continue
 
-                    xmap_inv[py, px] = (j0 * w0 + j1 * w1 + j2 * w2)
-                    ymap_inv[py, px] = (i0 * w0 + i1 * w1 + i2 * w2)
+                xmap_inv[py, px] = (j0 * w0 + j1 * w1 + j2 * w2)
+                ymap_inv[py, px] = (i0 * w0 + i1 * w1 + i2 * w2)
+                TN[py, px] += 1
 
-    # invert maps
-    # for i in range(dst_size):
-    #     for j in range(dst_size):
-    #         x, y = int(xmap[i, j]), int(ymap[i, j])
-    #         # x, y = int(round(mx0[i, j])), int(round(my0[i, j]))
-    #         # x = min(x+k, 47)
-    #         xmap_inv[y, x] = j
-    #         ymap_inv[y, x] = i
-
+    # if diagnostic:
+    #     return xmap_inv, ymap_inv, TN
+    # else:
     return xmap_inv, ymap_inv
+
